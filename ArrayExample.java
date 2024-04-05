d248b3c2513510aded6b3999f48bd50c57d5613d
vedakshari <challurivedakshari@gmail.com>
2024-04-05 19:26:39 +0530

    s

---
+++ ArrayExample.java
@@ -0,0 +1,11 @@
+
+public class ArrayExample{
+    public static void main(String args[])
+        {
+            String[] names={"hari","shanthi","seethal"};
+            for(int i=0;i<names.length;i++)
+            {
+            System.out.println(names[i]);
+        }
+        }
+}
